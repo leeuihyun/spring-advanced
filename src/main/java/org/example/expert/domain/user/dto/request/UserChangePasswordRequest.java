124ae15762b9e4ef20a0c2b4dda707f9bfb4d7e8
leeuihyun <meta212121@naver.com>
2025-06-05 15:12:45 +0900

    refactor : Lv3 RequestDto의 예외처리 Service 예외처리 -> Dto Valid 예외처리

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,8 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
-    @NotBlank
+
+    @Size(min = 8, message = "8자 이상만 작성 가능합니다.")
+    @Pattern(regexp = "(?=.*[A-Z])(?=.*\\d).+", message = "대문자, 숫자를 하나 이상 포함해야합니다.")
     private String newPassword;
 }
