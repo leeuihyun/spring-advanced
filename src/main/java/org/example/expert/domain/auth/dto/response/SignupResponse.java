6735239c4e24660451a2e99ab5233b13f014d85b
leeuihyun <meta212121@naver.com>
2025-06-11 20:20:05 +0900

    refactor : lombok 생성자 -> 수동 생성자로 변경 응답 객체 @JsonCreator

--- src/main/java/org/example/expert/domain/auth/dto/response/SignupResponse.java
+++ src/main/java/org/example/expert/domain/auth/dto/response/SignupResponse.java
@@ -1,11 +1,18 @@
 package org.example.expert.domain.auth.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
 public class SignupResponse {
 
-    private final Long userId;
+    private Long userId;
+
+    @JsonCreator
+    public SignupResponse(@JsonProperty("userId") Long userId) {
+        this.userId = userId;
+    }
 }
